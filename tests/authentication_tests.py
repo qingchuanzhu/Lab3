d8046125b5a63581d2acf052b651066a2f015269
Qingchuan Zhu <zqc@utexas.edu>
2015-11-06 11:35:20 -0600

    Added test for logout

--- tests/authentication_tests.py
+++ tests/authentication_tests.py
@@ -12,3 +12,7 @@ class StandAloneTests(TestCase):
 		""" Test the login function."""
 		mock_open.return_value.read.return_value = "george|bosco\n"
 		self.assertTrue(auth.login('george', 'bosco'))
+
+        def test_logout(self):
+            """Test the logout function...badly."""
+	    self.assertEqual(0,1)
